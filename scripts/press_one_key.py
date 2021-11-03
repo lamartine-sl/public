9e81b5f677da206aa8f9c275f98ef3538b84d4d1
moresconey <neymoresco@hotmail.com>
2021-11-03 09:45:21 -0300

    New script to Press Key

---
+++ scripts/press_one_key.py
@@ -0,0 +1,33 @@
+import time, random
+
+# Import keyboard package, if not installed will download
+try:
+    import keyboard
+except:
+    import os
+    os.system("pip install keyboard")
+    import keyboard
+
+# Function to exit script if user presses "q"
+def quit_script(seconds):
+    startTime = time.time()
+    while True:
+        if keyboard.is_pressed("esc"):
+            return True
+        elif time.time() - startTime > seconds:
+            return False
+
+# Print some instructions in the console
+print("Press the key you want to press in the script:")
+key_stroke = keyboard.read_key()
+
+print('Remember, to stop the script press the "Esc" key')
+
+# Wait 10 seconds 
+time.sleep(10)
+
+# Loop
+while True:
+    keyboard.press_and_release(key_stroke)
+    if quit_script(random.sample(range(4,8),1)[0]):
+        break
